d976f34778fb84be9b2defdfb697062a2705ac9f
simjsik <simjsik75@naver.com>
2024-10-01 19:32:05 +0900

    mobile_css_fixed

--- src/route/Login/Login.tsx
+++ src/route/Login/Login.tsx
@@ -64,7 +64,7 @@ const Login: React.FC = () => {
                         <button className="login_close_btn" onClick={handleLoginToggle}>닫기</button>
                         :
                         <button className="login_close_btn" onClick={handleLoginToggle}>
-                            <svg viewBox="-3 -3 24 24" className="login_undo_svg">
+                            <svg viewBox="-2 -3 24 24" className="login_undo_svg">
                                 <g id="prevIcon" data-name="prevIcon"><g id="prev" data-name="prev">
                                     <rect className="cls-1" width="18" height="18" />
                                     <polyline className="cls-2" points="11.57 3.87 6.43 9 11.56 14.13" />
